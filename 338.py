e631182ba37c314d009ebf192a12153a893e15e8
Raghav Mishra <raghav051006@gmail.com>
2026-04-25 21:31:40 +0530

    update 338.py

--- 338.py
+++ 338.py
@@ -1,3 +1,7 @@
+"""
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+"""
+
 class Solution(object):
     def countBits(self, n):
         """
@@ -5,16 +9,19 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        dp = [0] * (n + 1)
-        ans = [0]
+        dp = [0] * (n+1)
+        ans=[0]
         offset = 1
 
-        for i in range(1, n + 1):
-            if offset * 2 == i:
-                offset = i
+        for i in range(1,n+1):
+            if offset * 2 == i :
+                offset = i 
             dp[i] = 1 + dp[i - offset]
 
         return dp
+
+
+        
     
 # time complexity : O(n)
 # space complexity : O(n)
