1cd87f56c2efcc12a34c4f1df0a4f5c5c0613e8a
Raghav Mishra <raghav051006@gmail.com>
2026-05-20 20:29:44 +0530

    update 238.py

--- 238.py
+++ 238.py
@@ -1,20 +1,23 @@
+
 class Solution(object):
     def productExceptSelf(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
-        res = [1] * (len(nums))
+        res = [0] * (len(nums))
 
         prefix = 1
 
         for i in range(len(nums)):
-            res[i] = prefix
+            res[i] = prefix 
             prefix *= nums[i]
+            
+        postfix = 1 
 
-        postfix = 1
-
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
+        for i in range(len(nums) -1 , -1 , -1):
+            res[i] *= postfix 
             postfix *= nums[i]
-        return res
+        return res 
+
+        
